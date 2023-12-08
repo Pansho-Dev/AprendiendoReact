a4b92d1f21b2f4c0c7894eac790aaf5c58d8223f
Pansho_Dev_W <mr.franciscodeveloper@gmail.com>
2023-12-08 09:25:08 -0300

    Documentacion de useReduce y useMemo, y empezando a hablar de los contextos

--- src/components/ComponenteUseMemo.jsx
+++ src/components/ComponenteUseMemo.jsx
@@ -4,16 +4,25 @@ import React, { useMemo } from 'react'
 
 
 export const ComponenteUseMemo = ({list}) => {
-    
+        
+    // Creando una lista para inicializar los datos 
     list = [3,9,1,8,2]
 
+    // Ocupando use memo
     const sortedList = useMemo(() => {
+         
+        // Crear una copia de la lista
          const sorted = [...list];
+
+        // Ocupar metodo de array .Sort, para ordenar 
          sorted.sort();
+
+        // Regresamos el sorted ordenado y le colocamos la dependencia que hara que se ejecute el use memo
          return sorted;
          }, [list]);
   
   
+        // Luego en el el HTML lo que hacemos es
          return (
             <div>
              {sortedList.map(item => (
--- src/components/ComponenteUseReduce.jsx
+++ src/components/ComponenteUseReduce.jsx
@@ -1,8 +1,14 @@
 import React, { useReducer } from "react";
 
+
+// Inicializando valores
 const initialState = { count: 0, step: 1};
 
+// Tomar en consideracion que la funcion reducer es creada con el hook dentro del componente, la definicion de action es siguiente: 
+/* action: La acción realizada por el usuario. Puede ser un valor de cualquier tipo. Por convención,
+   una acción suele ser un objeto con una propiedad type que lo identifica y, opcionalmente, otras propiedades con información adicional. */
 function reducer(state, action) {
+  
   switch (action.type) {
     case "increment":
       return { count: state.count + state.step, step: state.step };
@@ -18,6 +24,9 @@ function reducer(state, action) {
 }
 
 export const ComponenteUseReduce = () => {
+  
+  // El dispatch se puede entender que sera el metodo o la funcion que despachara el valor del html ( del onClick),a la funcion reducer, pasandole
+  // el action.type, ejecutando el switch.
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
